87be35dea0beb175b59252ef56fcb2d6d203d725
Ikhlashmulya <nurahmanmulya@gmail.com>
2024-06-30 10:00:30 +0700

    add link in href in video list

--- resources/views/components/watch/video-list.blade.php
+++ resources/views/components/watch/video-list.blade.php
@@ -1,5 +1,5 @@
 <div class="flex flex-col mb-[8px] hover:scale-105 transition ease-in-out p-2 shadow-md rounded-md hover:shadow-lg">
-    <a href="" class="basis-[49%]"> <img class="w-full"
+    <a href="{{ url('/watch/' . $vidio->video_code) }}" class="basis-[49%]"> <img class="w-full"
             src="https://img.youtube.com/vi/{{ $vidio->video_code }}/0.jpg" alt="" srcset=""></a>
     <div class="basis-[49%] flex items-center mt-2">
         <a href="" class="text-sm font-semibold">{{ $vidio->judul }}</a>
