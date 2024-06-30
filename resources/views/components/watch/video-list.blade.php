ee362502582b90de515a35977c07d29ef1a4d536
Ikhlashmulya <nurahmanmulya@gmail.com>
2024-06-30 09:56:16 +0700

    change direction to flex-col in video-list

--- resources/views/components/watch/video-list.blade.php
+++ resources/views/components/watch/video-list.blade.php
@@ -1,7 +1,7 @@
-<div class="flex justify-between mb-[8px] hover:scale-105 transition ease-in-out p-2 shadow-md rounded-md hover:shadow-lg">
+<div class="flex flex-col mb-[8px] hover:scale-105 transition ease-in-out p-2 shadow-md rounded-md hover:shadow-lg">
     <a href="" class="basis-[49%]"> <img class="w-full"
             src="https://img.youtube.com/vi/{{ $vidio->video_code }}/0.jpg" alt="" srcset=""></a>
-    <div class="basis-[49%] flex items-center">
+    <div class="basis-[49%] flex items-center mt-2">
         <a href="" class="text-sm font-semibold">{{ $vidio->judul }}</a>
         {{-- <p class="text-sm mt-2">{{ $vidio->channel->nama }}</p> --}}
     </div>
