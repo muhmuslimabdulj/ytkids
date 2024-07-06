990069bc500bcd89ae3067ac631c5091272737d4
ddevfrostt <ddevfrostt@gmail.com>
2024-07-06 21:43:52 +0700

    add search feature to Home.Index.php

--- app/Livewire/Home/Index.php
+++ app/Livewire/Home/Index.php
@@ -4,17 +4,31 @@ namespace App\Livewire\Home;
 
 use App\Models\Channel;
 use App\Models\Video;
+use Livewire\Attributes\Url;
 use Livewire\Component;
 
 class Index extends Component
 {
+
     public $videos;
 
     public $channels;
 
+    #[Url]
+    public $search = "";
+
+    public function searchVid()
+    {
+        $this->videos = Video::query()->where('judul', 'like', '%' . $this->search . '%')->inRandomOrder()->get();
+    }
+
     public function render()
     {
-        $this->videos = Video::get();
+        if ($this->search) {
+            $this->searchVid();
+        } else {
+            $this->videos = Video::query()->inRandomOrder()->get();
+        }
         $this->channels = Channel::get();
         return view('livewire.home.index');
     }
