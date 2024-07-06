7862a4bfd089e3f38a3473ae347a1c8d2f1de2ac
ddevfrostt <ddevfrostt@gmail.com>
2024-07-06 21:46:10 +0700

    add wire:navigate attribute to watch.video-list component

--- resources/views/components/watch/video-list.blade.php
+++ resources/views/components/watch/video-list.blade.php
@@ -1,8 +1,7 @@
 <div class="flex flex-col mb-[8px] hover:scale-105 transition ease-in-out p-2 shadow-md rounded-md hover:shadow-lg">
-    <a href="{{ url('/watch/' . $vidio->video_code) }}" class="basis-[49%]"> <img class="w-full"
+    <a wire:navigate href="{{ url('/watch/' . $vidio->video_code) }}" class="basis-[49%]"> <img class="w-full"
             src="https://img.youtube.com/vi/{{ $vidio->video_code }}/0.jpg" alt="" srcset=""></a>
     <div class="basis-[49%] flex items-center mt-2">
         <a href="" class="text-sm font-semibold">{{ $vidio->judul }}</a>
-        {{-- <p class="text-sm mt-2">{{ $vidio->channel->nama }}</p> --}}
     </div>
 </div>
