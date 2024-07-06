b2a1867719c494b0c1edd2dc876cba664337e609
ddevfrostt <ddevfrostt@gmail.com>
2024-07-06 21:38:00 +0700

    add search feature, add wire:key attribute to home.channel component, change search button color

--- resources/views/components/home/navbar.blade.php
+++ resources/views/components/home/navbar.blade.php
@@ -1,7 +1,10 @@
-<div class="navbar bg-base-100 mt-5 flex flex-col sticky top-0 z-10">
-    <label class="w-full lg:w-[50%] input input-bordered relative items-center pe-0 rounded-full" id="searchBar">
-        <input type="text" placeholder="Search" />
-        <button class="btn btn-error rounded-e-full max-w-min absolute right-0">
+<div class="navbar bg-base-100 mt-2 flex flex-col sticky top-0 z-10">
+    <label
+        class="w-full lg:w-[50%] mt-3 input input-bordered relative items-center pe-0 rounded-full hover:scale-110 transition ease-in-out hover:shadow-lg border-none bg-zinc-100"
+        id="searchBar">
+        <input type="text" placeholder="Search" wire:model="search" wire:keydown.enter="searchVid"
+            class="border-3 border-gray-600" />
+        <button wire:click="searchVid" class="btn bg-[#D90B17] rounded-e-full max-w-min absolute right-0">
             <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="25" height="25" viewBox="0 0 50 50">
                 <path fill="#ffff"
                     d="M 21 3 C 11.601563 3 4 10.601563 4 20 C 4 29.398438 11.601563 37 21 37 C 24.355469 37 27.460938 36.015625 30.09375 34.34375 L 42.375 46.625 L 46.625 42.375 L 34.5 30.28125 C 36.679688 27.421875 38 23.878906 38 20 C 38 10.601563 30.398438 3 21 3 Z M 21 7 C 28.199219 7 34 12.800781 34 20 C 34 27.199219 28.199219 33 21 33 C 13.800781 33 8 27.199219 8 20 C 8 12.800781 13.800781 7 21 7 Z">
@@ -28,7 +31,7 @@
                     Beranda</p>
             </div>
             @foreach ($channels as $channel)
-                <x-home.channel :channel="$channel" />
+                <x-home.channel wire:key="{{ $channel->id }}" :channel="$channel" />
             @endforeach
         </div>
         <button id="rightArrow" class="bg-white border shadow-md text-slate-500 hover:scale-105 p-2 rounded-full">
