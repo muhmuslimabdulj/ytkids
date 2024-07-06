697f95f599634f293a467f6234b81ffb1472a097
ddevfrostt <ddevfrostt@gmail.com>
2024-07-06 21:47:09 +0700

    add Alpine JS library

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>{{ $title ?? 'YtFilt' }}</title>
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     @stack('plyr-js')
     @stack('plyr-css')
     @stack('truncate-css')
